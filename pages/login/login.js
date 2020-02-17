bcc747b2e53c340707fac66aa1b21657ccd27d9e
/获取当前登录的用户 <1461433958@qq.com>
2020-02-17 19:29:07 +0800

    123

--- pages/login/login.js
+++ pages/login/login.js
@@ -15,21 +15,21 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     console.log('清空用户信息和缓存')
     getApp().globalData.userInfo = {};
     wx.removeStorageSync('sj_userInfo');
@@ -40,52 +40,52 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  
-  bindKeyUserName: function (e) {
+
+  bindKeyUserName: function(e) {
     this.setData({
       userName: e.detail.value
     })
   },
 
-  bindKeyPassword: function (e) {
+  bindKeyPassword: function(e) {
     this.setData({
       password: e.detail.value
     })
   },
 
   // 登陆
-  login: function () {
+  login: function() {
     if (util.isBlank(this.data.userName)) {
       wx.showToast({
         title: '请输入用户名',
@@ -103,53 +103,59 @@ Page({
       title: '登录中',
     });
     userApi.login({
-      userName: this.data.userName,
-      // password: this.data.password
-      password: MD5.hexMD5(this.data.password)
-    })
-    .then((res) => {
-      console.log('登陆成功', res)
-      this.getToken(res.data);
-    })
-    .catch((error) => {
-      console.log('登陆失败', error)
-      wx.hideLoading();
-      wx.showToast({
-        title: error.message ? error.message : '登陆失败',
-        icon: 'none'
-      });
-    })
+        userName: this.data.userName,
+        // password: this.data.password
+        password: MD5.hexMD5(this.data.password)
+      })
+      .then((res) => {
+        console.log('登陆成功', res)
+        this.getToken(res.data);
+      })
+      .catch((error) => {
+        console.log('登陆失败', error)
+        wx.hideLoading();
+        wx.showToast({
+          title: error.message ? error.message : '登陆失败',
+          icon: 'none'
+        });
+      })
   },
   // 根据token获取用户信息
-  getToken: function (token) {
+  getToken: function(token) {
     userApi.token({
-      token: token
-    })
-    .then((res) => {
-      console.log('根据token获取用户信息成功', res)
-      wx.hideLoading();
-      var userInfo = res.data;
-      console.log('存储用户数据和设置缓存', userInfo)
-      getApp().globalData.userInfo = userInfo;
-      wx.setStorageSync("sj_userInfo", userInfo);
-      wx.setStorageSync("sj_userId", userInfo.id);
-      wx.setStorageSync("sj_token", userInfo.token);
-      wx.showToast({
-        title: '登录成功',
-        icon: 'success',
-        duration: 1000
+        token: token
       })
-      setTimeout(() => {
-        wx.navigateBack();
-      }, 1000);
-    })
-    .catch((error) => {
-      console.log('根据token获取用户信息失败', error)
-      wx.hideLoading();
-      wx.showToast({
-        title: error.message ? error.message : '根据token获取用户信息失败',
-        icon: 'none'
-      });
-    })
+      .then((res) => {
+        console.log('根据token获取用户信息成功', res)
+        wx.hideLoading();
+        var userInfo = res.data;
+        console.log('存储用户数据和设置缓存', userInfo)
+        getApp().globalData.userInfo = userInfo;
+        wx.setStorageSync("sj_userInfo", userInfo);
+        wx.setStorageSync("sj_userId", userInfo.id);
+        wx.setStorageSync("sj_token", userInfo.token);
+        wx.showToast({
+          title: '登录成功',
+          icon: 'success',
+          duration: 1000
+        })
+        setTimeout(() => {
+          if (getCurrentPages().length > 1) {
+            wx.navigateBack();
+          } else {
+            wx.switchTab({
+              url: '/pages/index/index',
+            })
+          }
+        }, 1000);
+      })
+      .catch((error) => {
+        console.log('根据token获取用户信息失败', error)
+        wx.hideLoading();
+        wx.showToast({
+          title: error.message ? error.message : '根据token获取用户信息失败',
+          icon: 'none'
+        });
+      })
   },
 })
