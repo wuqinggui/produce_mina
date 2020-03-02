218ccb65c2efd3b1759fdc76f5a26f9628323571
1461433958 <30288223+1461433958@users.noreply.github.com>
2020-03-02 14:16:31 +0800

    123

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,11 +1,14 @@
 // pages/register/register.js
+var shopApi = require('../../http/shopApi.js').default;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    regionList: [], // 地区列表
+    customerTypeArr: [], //客户类型
+    staffInfo: {}
   },
 
   /**
@@ -14,7 +17,115 @@ Page({
   onLoad: function (options) {
 
   },
+  // 选择客户类型
+  checkCustomerType: function (e) {
+    let { customerTypeArr, staffInfo } = this.data;
+    let index = e.detail.value;
+    ({ id: staffInfo.userRole, name: staffInfo.userRoleName } = customerTypeArr[index]);
+    this.setData({
+      staffInfo: staffInfo
+    });
+  },
+  // 选择地区
+  checkRegion(e) {
+    let { regionList, staffInfo } = this.data;
+    let index = e.detail.value;
+    ({id: staffInfo.regionId, name: staffInfo.regionName } = regionList[index]);
+    this.setData({
+      staffInfo: staffInfo
+    });
+  },
+  back: function() {
+    wx.navigateBack({});
+  },
+  registerHandle: function() {
+    let staffInfo = this.data.staffInfo;
+    // 邮箱正则匹配
+    let emailReg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    let toastTxt = '';
+    if (!staffInfo.phone) {
+      toastTxt = '请输入手机号';
+    } else if (!/^1[3|4|5|7|8]\d{9}$/.test(staffInfo.phone)) {
+      toastTxt = '手机号码格式，请重新输入';
+    } else if (!staffInfo.nickname) {
+      toastTxt = '请输入昵称';
+    } else if (!staffInfo.password) {
+      toastTxt = '请输入密码';
+    } else if (!staffInfo.enterPassword) {
+      toastTxt = '请输入确认密码';
+    } else if (staffInfo.password != staffInfo.enterPassword) {
+      toastTxt = '密码不一致，请重新输入';
+    } else if (!staffInfo.email) {
+      toastTxt = '请输入邮箱';
+    } else if (!emailReg.test(staffInfo.email)) {
+      toastTxt = '邮箱格式有误，请重新输入';
+    } else if (!staffInfo.regionId) {
+      toastTxt = '请选择地区';
+    } else if (!staffInfo.userRole) {
+      toastTxt = '请选择客户类型';
+    }
+    if (toastTxt) {
+      return wx.showToast({
+        title: toastTxt,
+        icon: 'none',
+        duration: 2000
+      })
+    }
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    })
+    staffInfo.userType = 3; //定为商家管理员
+    shopApi.addUser(staffInfo).then((res) => {
+      wx.hideLoading(); 
+      wx.showToast({
+        title: res.message ? res.message : '注册成功',
+        icon: 'success',
+        success: function() {
+          wx.navigateTo({
+            url: '/pages/login/login',
+          })
+        }
+      })
+    }).catch((error) => {
+      wx.hideLoading(); 
+      wx.showToast({
+        title: error.message ? error.message : '注册失败',
+        icon: 'none',
+        duration: 2000
+      })
+    })
 
+  },
+  bindAndSet: function (e) {
+    let key = e.currentTarget.dataset.key;
+    let staffInfo = this.data.staffInfo;
+    staffInfo[key] = e.detail.value;
+    this.setData({
+      staffInfo: staffInfo
+    })
+  },
+  // 获取地区列表
+  getRegionList () {
+    shopApi.region()
+      .then((res) => {
+        this.setData({
+          regionList: res.data ? res.data : [],
+        })
+      }).catch((error) => {
+        console.log(error);
+      }) 
+  },
+  // 获取客户类型
+  getCustomerType: function () {
+    shopApi.customertypeList().then((res) => {
+      this.setData({
+        customerTypeArr: res.data ? res.data : []
+      });
+    }).catch((error) => {
+      console.log(error);
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -26,7 +137,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getCustomerType(); //获取客户类型
+    this.getRegionList(); // 获取地区列表
   },
 
   /**
